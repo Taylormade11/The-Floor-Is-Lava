467bddf6a9f01fb7af3ff97e175370ea7f700998
paulritzman <ritzman.paul@gmail.com>
2018-04-23 15:12:50 -0700

    Fixed linking issue between app.js and index.html; Set canvas dimensions; Rendered initial character sprite in canvas

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var canvas = document.getElementById('game-screen');
+var ctx = canvas.getContext('2d');
+
+canvas.width = 750;
+canvas.height = 600;
+
+function drawSprite() {
+  ctx.fillStyle = 'red';
+  ctx.fillRect(canvas.width / 2, canvas.height / 2, 30, 30);
+}
+
+drawSprite();
