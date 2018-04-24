3f6c81beefbd32511b177d57c3fa5c2beb32a719
GoldbeardSea <timbusch1@gmail.com>
2018-04-24 09:18:52 -0700

    took out zombie code

--- js/audio.js
+++ js/audio.js
@@ -1,15 +1,4 @@
 'use strict';
-// window.onkeypress = function() {
-//   var key = window.onkeypress.event;
-//   if (key===32) {
-//     var test = new Audio('audio/336928__shnur__blip3 - Copy.wav');
-//     test.play();
-//   }
-// };
-
-// document.addEventListener('keydown', function(keyInput) {
-//   console.log(keyInput.which);
-// });
 
 window.onkeypress = function() {
   var jump = new Audio('audio/336928__shnur__blip3 - Copy.wav');
@@ -23,13 +12,3 @@ window.onkeypress = function() {
     }
   });
 };
-
-// window.onkeypress = function() {
-//   var sideways = new Audio('audio/124902__greencouch__beeps-231.wav');
-//   document.addEventListener('keydown', function(keyInput) {
-//     console.log(keyInput.which);
-//     if (keyInput.which ===37 || keyInput.which===39) {
-//       sideways.play();
-//     }
-//   });
-// };
