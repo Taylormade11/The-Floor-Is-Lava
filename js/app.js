5ffc6048ea5b7e467c83fee12d3eab3e15fc61b1
Mike K <m.kermes89@gmail.com>
2018-04-25 14:34:44 -0700

    added tile assets

--- js/app.js
+++ js/app.js
@@ -36,11 +36,12 @@ var levelMap = [
 
 function renderLevel(){
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle='#000000';
+  var tileSrc = document.getElementById('tiles');
+ // context.fillStyle='#000000';
   for(var i=0; i < levelRow; i++){
     for(var j=0; j <levelColumn; j++){
       if(levelMap[i][j]===1){
-        context.fillRect(j*tileSize, i*tileSize, tileSize, tileSize);
+        context.drawImage(tileSrc, j*tileSize, i*tileSize, tileSize, tileSize);
       }
     }
   }
