1dc5fd179d52ff243c6ffab14a56ace699d11d85
paulritzman <ritzman.paul@gmail.com>
2018-04-24 17:19:05 -0700

    altered code placement

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-//select the id for canvas to draw to
+// select the id for canvas to draw to
 var canvas = document.getElementById('game-screen');
-//sest the context of the canvas to 2d
+// sets the context of the canvas to 2d
 var context = canvas.getContext('2d');
 // size of the tiles (platforms) to be drawn
 var tileSize = 30;
@@ -46,18 +46,15 @@ function renderLevel(){
   }
 }
 
-CreateFloor();
-
+//CreateFloor();
 var ourSpriteCharacter;
 var gameFloors;
 var paused = false; // Game starts in a paused state
-
 var thud = new Audio('audio/jump.wav');
 var sideways = new Audio('audio/jump.wav');
 var jump = new Audio('audio/124902__greencouch__beeps-231.wav');
 
 // Starts the game by creating our Sprite, rendering the floor(s) & the start method of our gamescreen object.
-
 function startGame() {
   ourSpriteCharacter = new Sprite(30, 30, canvas.width / 2, canvas.height / 2);
   gameFloors = new CreateFloor(7150, 40, 0, 560);
@@ -66,7 +63,6 @@ function startGame() {
 }
 
 // Creates floor with parameters fed, may be able to feed it multiple blocks and compare all floors for object detection at one time.
-
 function CreateFloor(width, height, x, y) {
   var canvas = document.getElementById('game-screen');
   var ctx = canvas.getContext('2d');
@@ -77,7 +73,6 @@ function CreateFloor(width, height, x, y) {
 var jumpDelay = 0;
 
 // Grabs our game-screen canvas, sets h/w and context. Sets interval timing to run function every 25ms and event listeners on the entire window for events. Individual listeners at the bottom of the page for single button actions.
-
 var gameScreen = {
   canvas : document.getElementById('game-screen'),
   start : function() {
@@ -108,7 +103,6 @@ var gameScreen = {
 };
 
 // Creates sprite with inputs we feed it, and gives it an update method and updatedPos method. Also applies speeds and gravity effects.
-
 function Sprite(width, height, x, y) {
   this.width = width;
   this.height = height;
@@ -127,22 +121,15 @@ function Sprite(width, height, x, y) {
     this.x += this.speedX;
     this.gravitySpeed += this.gravity;
     this.y += this.speedY + this.gravitySpeed;
-    // Trying to control/set a limit on how fast sprite can travel in y axis up and down. But once it reaches that speed it stays that speed and won't change direction. Need to go about differently, possibly globally
-    // if (this.speedY >= 7) {
-    //   this.speedY = 7;
-    // } else if (this.speedY <= -50) {
-    //   this.speedY = -50;
-    // }
   };
 }
 
 // Looks for a collision between the Sprite y location, if it reaches where the edge of the floor is drawn it console logs a loss message and prompts alert and stops the updating... or form to enter name into for highscore?
-
 function collision() {
   if (ourSpriteCharacter.y > 560) {
     console.log('sorry you hit the lava, you lose');
     gameScreen.stop();
-    thud.play();
+    // thud.play();
     alert('sorry you hit the lava, you lose');
   } else {
     console.log('no collision with floor detected yet');
@@ -161,8 +148,6 @@ function togglePause() {
 }
 
 // updates game-screen and clears old images so it isn't drawing lines with the past square's locations. Listens for A & D or Left and Right arrows for X axis movement. Listens for spacebar for jump / negative Y movement. Every time you jump it sets the Jump delay to 400 ms and then each clear loop decrements the jump delay 25ms until it is 0 again. Can not jump unless jumpDelay is back to 0. Redraws floor because of the clear, but we can only clear above the floor with the right measurements so it only has to be drawn once.
-
-
 function updateGameArea() {
   renderLevel();
   gameScreen.clear();
@@ -170,17 +155,17 @@ function updateGameArea() {
   var baseRow = Math.floor(ourSpriteCharacter.y/tileSize);
   var colOverlap = ourSpriteCharacter.x%tileSize;
   var rowOverlap = ourSpriteCharacter.y%tileSize;
-  if (gameScreen.pressed[37]) {ourSpriteCharacter.speedX = -3.5;
-    sideways.play(); }
-  if (gameScreen.pressed[65]) {ourSpriteCharacter.speedX = -3.5;
-    sideways.play(); }
-  if (gameScreen.pressed[39]) {ourSpriteCharacter.speedX = 3.5;
-    sideways.play();}
-  if (gameScreen.pressed[68]) {ourSpriteCharacter.speedX = 3.5;
-    sideways.play();}
+  if (gameScreen.pressed[37]) {ourSpriteCharacter.speedX = -3.5;}
+  //  sideways.play(); }
+  if (gameScreen.pressed[65]) {ourSpriteCharacter.speedX = -3.5;}
+  //  sideways.play(); }
+  if (gameScreen.pressed[39]) {ourSpriteCharacter.speedX = 3.5;}
+  //  sideways.play();}
+  if (gameScreen.pressed[68]) {ourSpriteCharacter.speedX = 3.5;}
+  //  sideways.play();}
   if (jumpDelay === 0 && gameScreen.pressed[32]) {
     ourSpriteCharacter.speedY += -10;
-    jump.play();
+    //  jump.play();
     jumpDelay += 1200;
     console.log('jump recorded, now wait a little bit before you can jump again so you don\'t cheat and fly through the level!');
   }
@@ -199,19 +184,17 @@ function updateGameArea() {
   if(ourSpriteCharacter.speedX>0){
     if((levelMap[baseRow][baseCol+1] && !levelMap[baseRow][baseCol]) || (levelMap[baseRow+1][baseCol+1] && !levelMap[baseRow+1][baseCol] && rowOverlap)){
       ourSpriteCharacter.x=baseCol*tileSize;
-      // ourSpriteCharacter.speedY += ourSpriteCharacter.gravitySpeed;
+      console.log(ourSpriteCharacter.x=baseCol*tileSize);
     }
   }
 
   if(ourSpriteCharacter.speedX<0){
     if((!levelMap[baseRow][baseCol+1] && levelMap[baseRow][baseCol]) || (!levelMap[baseRow+1][baseCol+1] && levelMap[baseRow+1][baseCol] && rowOverlap)){
       ourSpriteCharacter.x=(baseCol+1)*tileSize;
-      // ourSpriteCharacter.speedY += ourSpriteCharacter.gravitySpeed;
     }
   }
 
   // checking for vertical collisions in downward but not upwards so we can jump through them.
-
   baseCol = Math.floor(ourSpriteCharacter.x/tileSize);
   baseRow = Math.floor(ourSpriteCharacter.y/tileSize);
   colOverlap = ourSpriteCharacter.x%tileSize;
@@ -223,7 +206,6 @@ function updateGameArea() {
     }
   }
 }
-
 
 startGame();
 renderLevel();
