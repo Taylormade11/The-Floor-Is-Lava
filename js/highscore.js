4ec0512bc0eec6eec779515a75a4c4b3a23ad8ae
GoldbeardSea <timbusch1@gmail.com>
2018-04-26 16:22:21 -0700

    array of highscore functional

---
+++ js/highscore.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var firstFill = document.getElementById('firstScore');
+var secondFill = document.getElementById('secondScore');
+var thirdFill = document.getElementById('thirdScore');
+var fourthFill = document.getElementById('fourthScore');
+var fifthFill = document.getElementById('fifthScore');
+var sixthFill = document.getElementById('sixthScore');
+var seventhFill = document.getElementById('seventhScore');
+
+var highScore = {
+  1650000: 'MF',
+  1350000: 'AB',
+  1750000: 'AG',
+  1100000: 'CL',
+  1000000: 'CM',
+  900000: 'JF',
+  700000: 'AAA',
+
+};
+var sortable = [];
+for (var initials in highScore) {
+  sortable.push([parseInt(initials), highScore[parseInt(initials)]]);
+}
+
+var storeGoal = localStorage.getItem('local-score');
+var storeInitial= localStorage.getItem('local-user-initials');
+sortable.push([parseInt(storeGoal), storeInitial]);
+sortable.sort(function(a, b){return b[0] - a[0];});
+
+function tableContent() {
+  firstFill.textContent = sortable[0][0] + ' ' + sortable[0][1];
+  secondFill.textContent = sortable[1][0] + ' ' + sortable[1][1];
+  thirdFill.textContent = sortable[2][0] + ' ' + sortable[2][1];
+  fourthFill.textContent = sortable[3][0] + ' ' + sortable[3][1];
+  fifthFill.textContent = sortable[4][0] + ' ' + sortable[4][1];
+  sixthFill.textContent = sortable[5][0] + ' ' + sortable[5][1];
+  seventhFill.textContent = sortable[6][0] + ' ' + sortable[6][1];
+}
+
+tableContent();
+
